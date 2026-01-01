718b5e952a535a523890056ae32041a735d3e6f0
FAYnim <yuana6898>
2026-01-01 15:56:41 +0700

    feat: implementation of admin topup option

--- reply/admin-topup.php
+++ reply/admin-topup.php
@@ -3,7 +3,7 @@
 $update_result = updateUserPosition($chat_id, 'main', '');
 
 if (!$update_result) {
-    $bot->sendMessage($chat_id, "❌ Something Error!");
+    $bot->sendMessage($chat_id, "❌ Something Error 1!");
     return;
 }
 
@@ -16,11 +16,43 @@ if (empty($admin)) {
 	return;
 }
 
+$admin_msg_id = $bot->getCallbackMessageId();
+if(!$admin_msg_id) {
+    $bot->sendMessage($chat_id, "❌ Something Error 2!");
+    return;
+}
+
+if($admin_msg_id != $msg_id) {
+	// Update msg_id column with $admin_msg_id in table users and admins
+	db_update('smm_users', ['chatid' => $chat_id], ['msg_id' => $admin_msg_id]);
+	db_update('smm_admins', ['chatid' => $chat_id], ['msg_id' => $admin_msg_id]);
+}
+
+// Get user_chat_id
+$parts = explode('_', $cb_data);
+$user_chat_id = end($parts);
+
 if(strpos($cb_data, "approve") !== false) {
-	$bot->sendMessage($chat_id, "Approve");
+
+	$update_result = updateUserPosition($chat_id, 'main', 'topup_approve_'.$user_chat_id);
+	if (!$update_result) {
+		$bot->sendMessage($chat_id, "❌ Something Error!");
+		return;
+	}
+
+	$bot->editMessage($chat_id, $admin_msg_id,  "✅ Berapa nominal topup yang disetujui?");
+
 } elseif (strpos($cb_data, "reject")) {
-	$bot->sendMessage($chat_id, "Reject");
+
+	$update_result = updateUserPosition($chat_id, 'main', 'topup_reject_'.$user_chat_id);
+	if (!$update_result) {
+		$bot->sendMessage($chat_id, "❌ Something Error!");
+		return;
+	}
+
+	$bot->editMessage($chat_id, $admin_msg_id, "❌ Kenapa topup ini ditolak?");
+
 } else {
-	$bot->sendMessage($chat_id, "Systek Error!");
+	$bot->sendMessage($chat_id, "System Error!");
 }
 ?>
