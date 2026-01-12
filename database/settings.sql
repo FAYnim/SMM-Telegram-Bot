bd282545baa9acc39ad93c0fbd17c43953b78fee
FAYnim <yuana6898@gmail.com>
2026-01-13 04:46:27 +0700

    feat: setting withdraw data
    updated files: settings.sql

--- database/settings.sql
+++ database/settings.sql
@@ -1,5 +1,9 @@
+TRUNCATE smm_settings;
+
 INSERT INTO smm_settings (category, setting_key, setting_value, description) VALUES
 ('payment', 'dana_number', '0812-3456-7890', 'Nomor DANA untuk topup'),
 ('payment', 'dana_name', 'Admin SMM', 'Nama pemilik akun DANA'),
 ('payment', 'shopeepay_number', '0812-3456-7890', 'Nomor ShopeePay untuk topup'),
-('payment', 'shopeepay_name', 'Admin SMM', 'Nama pemilik akun ShopeePay')
+('payment', 'shopeepay_name', 'Admin SMM', 'Nama pemilik akun ShopeePay'),
+('withdraw', 'min_withdraw', '50000', 'Minimum jumlah withdrawal'),
+('withdraw', 'admin_fee', '5000', 'Biaya admin withdrawal');
