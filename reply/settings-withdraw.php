1d0e14dca6fea3c56b5cf7c611942a2d53d49f02
FAYnim <yuana6898@gmail.com>
2026-01-19 18:54:14 +0700

    feat: fee type conditional - persentage/flat
    updated files: settings-withdraw

--- reply/settings-withdraw.php
+++ reply/settings-withdraw.php
@@ -21,7 +21,14 @@ $reply .= "<b>📊 Minimum Withdrawal</b>\n";
 $reply .= "Nilai: Rp " . number_format(($withdraw_data['min_withdraw'] ?? 0), 0, ',', '.') . "\n\n";
 
 $reply .= "<b>💰 Biaya Admin</b>\n";
-$reply .= "Nilai: Rp " . number_format(($withdraw_data['admin_fee'] ?? 0), 0, ',', '.') . "\n\n";
+$admin_fee_type = $withdraw_data['admin_fee_type'] ?? 'flat';
+$admin_fee_value = $withdraw_data['admin_fee'] ?? 0;
+
+if($admin_fee_type == 'percentage') {
+	$reply .= "Tipe: Persentase | Nilai: " . $admin_fee_value . "%\n\n";
+} else {
+	$reply .= "Tipe: Flat | Nilai: Rp " . number_format($admin_fee_value, 0, ',', '.') . "\n\n";
+}
 
 $reply .= "<i>Pilih pengaturan yang ingin diubah:</i>";
 
